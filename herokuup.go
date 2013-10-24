b50f9f1f5b08d34c5b84a85d661991334e5b1a57
Frank Liu <gniquil@gmail.com>
2013-10-23 18:16:11 -0700

    added email

--- herokuup.go
+++ herokuup.go
@@ -4,13 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/smtp"
 	"os"
 )
 
 type Config struct {
 	Checks         []string `json:"checks"`
 	FromEmail      string   `json:"from_email"`
-	ToEmail        string   `json:"to_email"`
+	ToEmails       []string `json:"to_emails"`
 	SendOnlyOnFail bool     `json:"send_only_on_fail"`
 }
 
@@ -23,26 +24,58 @@ func main() {
 		go Check(url, channel)
 	}
 
-	totalFailed := 0
+	responses := []map[string]int{}
+
 	for i := 0; i < len(config.Checks); i++ {
 		checkResponse := <-channel
-		for _, val := range checkResponse {
+		responses = append(responses, checkResponse)
+	}
+
+	message := CraftMessage(responses)
+
+	err := smtp.SendMail(
+		"localhost:25",
+		nil,
+		config.FromEmail,
+		config.ToEmails,
+		[]byte(message),
+	)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func CraftMessage(responses []map[string]int) string {
+	message := ""
+	totalFailed := 0
+	for _, response := range responses {
+		for key, val := range response {
 			if val != 200 {
+				message += fmt.Sprintf("%s returned status code %d\n", key, val)
 				totalFailed++
 			}
 		}
 	}
-
-	fmt.Printf("%d out of %d failed!", totalFailed, len(config.Checks))
+	var finalMessage string
+	if totalFailed > 0 {
+		finalMessage = fmt.Sprintf("Subject: [herokuup] %d urls are down!\n\n", totalFailed)
+		finalMessage += message + "\n"
+		finalMessage += fmt.Sprintf("%d out of %d failed.", totalFailed, len(responses))
+	} else {
+		finalMessage = "Subject: [herokuup] All urls are up!\n\n"
+		finalMessage += fmt.Sprintf("%d out of %d passed.", len(responses), len(responses))
+	}
+	finalMessage += "\n"
+	return finalMessage
 }
 
 func Check(url string, channel chan<- map[string]int) {
 	res, err := http.Get(url)
 	if err != nil {
-		fmt.Println(err)
+		// fmt.Println(err)
 		channel <- map[string]int{url: 0}
 	} else {
-		fmt.Println(url, res.StatusCode)
+		// fmt.Println(url, res.StatusCode)
 		channel <- map[string]int{url: res.StatusCode}
 	}
 }
